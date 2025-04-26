c5e5cdc75bce9b2a14f348381c0491a81f304970
ConniConni <nmtgktmo@gmail.com>
2025-04-26 12:05:25 +0900

    optionをmap関数を用いてリファクタリング

--- react-guide-material/06_control_and_form/src/120_select/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/120_select/start/Example.jsx
@@ -6,10 +6,17 @@ const Example = () => {
   const selectedVal = (e) => setSelected(e.target.value);
   return (
     <>
-      <select value={selected} onChange={selectedVal}>
+      {/* <select value={selected} onChange={selectedVal}>
         <option value="Apple">Apple</option>
         <option value="Banana">Banana</option>
         <option value="Cherry">Cherry</option>
+      </select> */}
+      <select value={selected} onChange={selectedVal}>
+        {COLLECTION.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
       <div>選択された果実:{selected}</div>
     </>
