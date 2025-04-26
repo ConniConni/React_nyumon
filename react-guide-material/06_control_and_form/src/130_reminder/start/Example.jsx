8e20d4575da44e44b6f9449593fcd2abfda6ffa1
ConniConni <nmtgktmo@gmail.com>
2025-04-26 16:50:05 +0900

    画面表示実装

--- react-guide-material/06_control_and_form/src/130_reminder/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/130_reminder/start/Example.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+
 const Example = () => {
+  const todosList = [
+    {
+      id: 1,
+      content: "店予約する",
+    },
+    {
+      id: 2,
+      content: "卵買う",
+    },
+    {
+      id: 3,
+      content: "郵便出す",
+    },
+  ];
+  const [todoList, setTodoList] = useState(todosList);
+  const todoCompletion = () => {};
+
   return (
     <>
-      <h3>完成系と同じような機能を作成してください。</h3>
-      <p>また、枠線で囲まれた部品をコンポーネントに分離してください。</p>
+      <h2>Reminder</h2>
+      <div>
+        {todoList.map((list) => {
+          return (
+            <div key={list.id}>
+              <button id={list.id} value={list.id} onClick={todoCompletion}>
+                完了
+              </button>
+              <label htmlFor={list.id}>{list.content}</label>
+            </div>
+          );
+        })}
+      </div>
+      <input type="text" />
+      <button>追加</button>
     </>
   );
 };
