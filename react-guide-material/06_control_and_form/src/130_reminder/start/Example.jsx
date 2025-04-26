ef49b5483386225cbe5498932910fdb029910d8b
ConniConni <nmtgktmo@gmail.com>
2025-04-26 23:26:14 +0900

    コンポーネントを切り分け、リファクタリング

--- react-guide-material/06_control_and_form/src/130_reminder/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/130_reminder/start/Example.jsx
@@ -1,69 +1,10 @@
-import { useState } from "react";
+import Todo from "./components/Todo";
 
 const Example = () => {
-  const todosList = [
-    {
-      id: 1,
-      content: "店予約する",
-    },
-    {
-      id: 2,
-      content: "卵買う",
-    },
-    {
-      id: 3,
-      content: "郵便出す",
-    },
-  ];
-
-  const [todos, setTodos] = useState(todosList);
-  const [enterTodo, setEnterTodo] = useState("");
-
-  const deleteTodo = (id) => {
-    console.log(id);
-    // stateの保持するidとクリックしたボタンが保持するidが一致しないものを表示
-    const newTodos = todos.filter((todo) => {
-      return todo.id !== id;
-    });
-
-    setTodos(newTodos);
-  };
-
-  const addTodo = () => {
-    const newTodo = {
-      id: Math.floor(Math.random() * 1e5),
-      content: enterTodo,
-    };
-    setTodos([...todos, newTodo]);
-    setEnterTodo("");
-    console.log(todos);
-  };
-
   return (
     <>
       <h2>Reminder</h2>
-      <div>
-        {todos.map((todo) => {
-          return (
-            <div key={todo.id}>
-              <button
-                id={todo.id}
-                value={todo.id}
-                onClick={() => deleteTodo(todo.id)}
-              >
-                完了
-              </button>
-              <label htmlFor={todo.id}>{todo.content}</label>
-            </div>
-          );
-        })}
-      </div>
-      <input
-        type="text"
-        value={enterTodo}
-        onChange={(e) => setEnterTodo(e.target.value)}
-      />
-      <button onClick={addTodo}>追加</button>
+      <Todo />
     </>
   );
 };
---
+++ react-guide-material/06_control_and_form/src/130_reminder/start/components/List.jsx
@@ -0,0 +1,25 @@
+const List = ({ todos, deleteTodo }) => {
+  const complete = (id) => {
+    deleteTodo(id);
+  };
+  return (
+    <div>
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <button
+              id={todo.id}
+              value={todo.id}
+              onClick={() => complete(todo.id)}
+            >
+              完了
+            </button>
+            <label htmlFor={todo.id}>{todo.content}</label>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
