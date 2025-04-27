c30e9e4d7cc249230f409318ee80fd0a1ba4c4ae
ConniConni <nmtgktmo@gmail.com>
2025-04-27 13:12:55 +0900

    Todoコンポーネント読み込み

--- react-guide-material/06_control_and_form/src/130_reminder/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/130_reminder/start/Example.jsx
@@ -1,5 +1,11 @@
+import Todo from "./components/Todo";
 const Example = () => {
-  return <h2>Remainder</h2>;
+  return (
+    <>
+      <h2>Remainder</h2>
+      <Todo />
+    </>
+  );
 };
 export default Example;
 
