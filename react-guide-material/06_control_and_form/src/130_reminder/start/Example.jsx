85819170ab1def3938cad59717645df2dcd09b60
ConniConni <nmtgktmo@gmail.com>
2025-04-27 12:54:38 +0900

    ２回目のためにリセット

--- react-guide-material/06_control_and_form/src/130_reminder/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/130_reminder/start/Example.jsx
@@ -1,12 +1,12 @@
-import Todo from "./components/Todo";
+// import Todo from "./components/Todo";
 
-const Example = () => {
-  return (
-    <>
-      <h2>Reminder</h2>
-      <Todo />
-    </>
-  );
-};
+// const Example = () => {
+//   return (
+//     <>
+//       <h2>Reminder</h2>
+//       <Todo />
+//     </>
+//   );
+// };
 
-export default Example;
+// export default Example;
--- react-guide-material/06_control_and_form/src/130_reminder/start/components/Form.jsx
+++ react-guide-material/06_control_and_form/src/130_reminder/start/components/Form.jsx
@@ -1,25 +1,25 @@
-import { useState } from "react";
-const Form = ({ createTodo }) => {
-  const [enterTodo, setEnterTodo] = useState("");
+// import { useState } from "react";
+// const Form = ({ createTodo }) => {
+//   const [enterTodo, setEnterTodo] = useState("");
 
-  const addTodo = () => {
-    const newTodo = {
-      id: Math.floor(Math.random() * 1e5),
-      content: enterTodo,
-    };
-    //setTodos([...todos, newTodo]);
-    createTodo(newTodo);
-    setEnterTodo("");
-  };
-  return (
-    <div>
-      <input
-        type="text"
-        value={enterTodo}
-        onChange={(e) => setEnterTodo(e.target.value)}
-      />
-      <button onClick={addTodo}>追加</button>
-    </div>
-  );
-};
-export default Form;
+//   const addTodo = () => {
+//     const newTodo = {
+//       id: Math.floor(Math.random() * 1e5),
+//       content: enterTodo,
+//     };
+//     //setTodos([...todos, newTodo]);
+//     createTodo(newTodo);
+//     setEnterTodo("");
+//   };
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         value={enterTodo}
+//         onChange={(e) => setEnterTodo(e.target.value)}
+//       />
+//       <button onClick={addTodo}>追加</button>
+//     </div>
+//   );
+// };
+// export default Form;
