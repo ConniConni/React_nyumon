bd69e2e539eee8efa9c3190444fe4dbe089808ba
ConniConni <nmtgktmo@gmail.com>
2025-04-27 20:02:19 +0900

    ３回目実施のためにコメントアウト

--- react-guide-material/06_control_and_form/src/130_reminder/start/components/List.jsx
+++ react-guide-material/06_control_and_form/src/130_reminder/start/components/List.jsx
@@ -1,39 +1,14 @@
-const List = ({ todoList, deleteTodo }) => {
-  const complete = (id) => deleteTodo(id);
+// const List = ({ todoList, deleteTodo }) => {
+//   const complete = (id) => deleteTodo(id);
 
-  return (
-    <div>
-      {todoList.map((todo) => {
-        return (
-          <div key={todo.id}>
-            <button
-              id={todo.list}
-              value={todo.list}
-              onClick={() => complete(todo.id)}
-            >
-              完了
-            </button>
-            <label htmlFor={todo.id}>{todo.content}</label>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
-export default List;
-
-// const List = ({ todos, deleteTodo }) => {
-//   const complete = (id) => {
-//     deleteTodo(id);
-//   };
 //   return (
 //     <div>
-//       {todos.map((todo) => {
+//       {todoList.map((todo) => {
 //         return (
 //           <div key={todo.id}>
 //             <button
-//               id={todo.id}
-//               value={todo.id}
+//               id={todo.list}
+//               value={todo.list}
 //               onClick={() => complete(todo.id)}
 //             >
 //               完了
@@ -45,5 +20,30 @@ export default List;
 //     </div>
 //   );
 // };
-
 // export default List;
+
+// // const List = ({ todos, deleteTodo }) => {
+// //   const complete = (id) => {
+// //     deleteTodo(id);
+// //   };
+// //   return (
+// //     <div>
+// //       {todos.map((todo) => {
+// //         return (
+// //           <div key={todo.id}>
+// //             <button
+// //               id={todo.id}
+// //               value={todo.id}
+// //               onClick={() => complete(todo.id)}
+// //             >
+// //               完了
+// //             </button>
+// //             <label htmlFor={todo.id}>{todo.content}</label>
+// //           </div>
+// //         );
+// //       })}
+// //     </div>
+// //   );
+// // };
+
+// // export default List;
