246f2a570c1cf6cefaee5e53c44141e9cc0d1dc8
ConniConni <nmtgktmo@gmail.com>
2025-05-19 14:40:40 +0900

    復習のためリセット

--- react-guide-material/08_other_function/src/060_practice_ref/start/Example.jsx
+++ react-guide-material/08_other_function/src/060_practice_ref/start/Example.jsx
@@ -1,10 +1,7 @@
 import { useState, useRef, forwardRef, useImperativeHandle } from "react";
 
 const Video = forwardRef(({ path }, ref) => {
-
-  useImperativeHandle(ref, () => ({
-    
-  }));
+  useImperativeHandle(ref, () => ({}));
 
   return (
     <video style={{ maxWidth: "100%" }}>
@@ -21,8 +18,9 @@ const Example = () => {
   return (
     <div>
       <h3>練習問題</h3>
-      <p>useRef、useImperativeHandle、forwardRefを使って完成系の動画再生機能を作成してください。※useImperativeHandleでplay(再生)、stop(停止)メソッドを定義すること。
-</p>
+      <p>
+        useRef、useImperativeHandle、forwardRefを使って完成系の動画再生機能を作成してください。※useImperativeHandleでplay(再生)、stop(停止)メソッドを定義すること。
+      </p>
       <Video ref={ref} path="./sample.mp4" />
       <button
         onClick={() => {
